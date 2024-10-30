f7ee3a3ce71246995d52376944f4f7dfa2f085cf
James Wadley <86320001+w4dd325@users.noreply.github.com>
2024-10-30 19:59:06 +0000

    add wcag22aa rules

--- cypress/e2e/accessibility-test.cy.js
+++ cypress/e2e/accessibility-test.cy.js
@@ -2,12 +2,16 @@ describe('Accessibility Test', () => {
 
   it('should check accessibility of the homepage', { defaultCommandTimeout: 15000 }, () => {
     cy.visit('https://example.cypress.io/');
-    cy.checkAccessibility();
+    cy.checkAccessibility(null, {
+      runOnly: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag2a', 'wcag22aa', 'best-practice']
+    });
   });
 
   it('should check accessibility of the querying page', { defaultCommandTimeout: 15000 }, () => {
     cy.visit('https://example.cypress.io/commands/querying');
-    cy.checkAccessibility();
+    cy.checkAccessibility(null, {
+      runOnly: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag2a', 'wcag22aa', 'best-practice']
+    });
   });
-  
+
 });
